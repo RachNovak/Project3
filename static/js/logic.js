404d73d7f3ea910af29874ef9a2713ca47c46e3e
Martha <martha.griggs9@gmail.com>
2023-04-18 20:45:05 -0500

    updating charts

--- static/js/logic.js
+++ static/js/logic.js
@@ -44,8 +44,8 @@ function init() {
         
         var firstCountry = data[0];
         console.log(firstCountry)
-        buildCharts(firstCountry);
-        buildMetadata(firstCountry);
+        buildMetadata(firstCountry.country);
+        buildCharts(firstCountry.country);
 
         
     
@@ -84,9 +84,84 @@ function buildMetadata(row) {
   });
 }
 
+// 1. Create the buildCharts function.
+function buildCharts(row) {
+    // 2. Use d3.json to load and retrieve the emissions file 
+    d3.json("http://127.0.0.1:5000/api/v1.0/finalemissions").then((data) => {
+      // 3. Create a variable that holds the emissions array. 
+      var sample_country = data;
+      // 4. Create a variable that filters the samples for the object with the desired country
+      var filter_data = sample_country.filter(sampleObj => sampleObj.country == row);
+      //console.log(filter_data)
+      //  5. Create a variable that holds the first sample in the array.
+      var filter_sample_array = filter_data[0]
+      
+      //remove country in order to chart
+      var no_country = filter_sample_array
+      delete no_country.country
+      var no_total = no_country
+      delete no_total.total
+
+    //   Object.entries(filter_sample_array).forEach(([key, value]) => {
+
+    //   })
+     
+      let values = Object.values(no_total)
+      let keys = Object.keys(no_total)
+
+      // 8. Create the trace for the bar chart. 
+      var trace = {
+        x: keys,
+        y: values,
+        type: "bar",
+        marker: {
+            colors: "purple"
+        },
+        hovertext: values,
+        //color: 
+      }
+  
+      bardata = [trace]
+      // 9. Create the layout for the bar chart. 
+      var layout = {
+        title: "Emissions by Type",
+        hovermode: "closest", 
+        height: 500,
+        width: 500
+      }
+      // 10. Use Plotly to plot the data with the layout. 
+      Plotly.newPlot("bar", bardata, layout)
+
+      var ultimateColors = [
+        ['rgb(37,52,148)', 'rgb(44,127,184)', 'rgb(65,182,196)', 'rgb(161,218,180)', 'rgb(255,255,204)']
+      ];
 
 
+      var piedata = [{
+        values: values,
+        labels: keys,
+        type: 'pie',
+        marker: {
+            colors: ultimateColors[0]
+        }
+      }];
 
+      let layout2 = {
+        height:500, 
+        width: 700, 
+        title: "Emissions by Type"
+      };
+
+      Plotly.newPlot("gauge", piedata, layout2);
+
+
+      //Filter the data for the object with the desired sample number
+      var metadata = data;
+      var resultArray = metadata.filter(sampleObj => sampleObj.country == row);
+      var result = resultArray[0];
+      });
+  
+    }
 
 
     
