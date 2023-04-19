ce391116420ad0b8528ab7a35d36791aa1d1d888
MikaMohd <120360328+MikaMohd@users.noreply.github.com>
2023-04-19 00:04:20 -0500

    updating the branch

---
+++ app.py
@@ -0,0 +1,124 @@
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+import psycopg2
+from flask import Flask, jsonify
+from flask_cors import CORS
+import numpy as np
+import pandas as pd
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+CORS(app)
+
+###############################################
+# Database Setup
+###############################################
+## declare the connection string specifying the hose name database name, user name and password
+conn_string = "host= 'localhost' dbname= 'project3'\
+user= 'postgres' password= '3LetterAnatomy'"
+
+#use connect function to establish the connection to the database
+conn = psycopg2.connect(conn_string)
+
+#Query all records in the database
+data = pd.read_sql('select * from emissions', conn)
+
+
+#################################################
+# Flask Routes
+#################################################
+@app.route("/")
+#Create function for welcome page
+def welcome():
+    return(
+    
+    f"Welcome to the CO2 Emissions by Country API!<br/>"
+    f"Available Routes:<br/>"
+    f"/api/v1.0/finalemissions<br/>"
+    f"/api/v1.0/totalemissions<br/>"
+    f"/api/v1.0/coal<br/>"
+    f"/api/v1.0/oil<br/>"
+    f"/api/v1.0/gas<br/>"
+    f"/api/v1.0/cement<br/>"
+    f"/api/v1.0/flaring<br/>"
+    )
+
+#Create function for total page
+@app.route("/api/v1.0/finalemissions")
+def final():
+    
+    conn = psycopg2.connect(conn_string)
+    
+    final = pd.read_sql('select * from final', conn)
+    
+   
+    return final.to_json(orient = 'records')
+    # return jsonify(total.loc[:, ['country', 'total']])
+    # return jsonify(list(total_dictionary))
+
+
+#Create function for total page
+@app.route("/api/v1.0/totalemissions")
+def total():
+    
+    conn = psycopg2.connect(conn_string)
+    
+    total = pd.read_sql('select * from total', conn)
+    
+   
+    return total.to_json(orient = 'records')
+    # return jsonify(total.loc[:, ['country', 'total']])
+    # return jsonify(list(total_dictionary))
+
+#Create function for coal page
+@app.route("/api/v1.0/coal")
+def coal():
+    conn = psycopg2.connect(conn_string)
+    
+    coal = pd.read_sql('select * from coal', conn)
+    
+    return coal.to_json(orient = 'records')
+
+#Create function for oil page
+@app.route("/api/v1.0/oil")
+def oil():
+    conn = psycopg2.connect(conn_string)
+    
+    oil = pd.read_sql('select * from oil', conn)
+    
+    return oil.to_json(orient = 'records')
+
+#Create function for gas page
+@app.route("/api/v1.0/gas")
+def gas():
+    conn = psycopg2.connect(conn_string)
+    
+    gas = pd.read_sql('select * from gas', conn)
+    
+    return gas.to_json(orient = 'records')
+
+#Create function for cement page
+@app.route("/api/v1.0/cement")
+def cement():
+    conn = psycopg2.connect(conn_string)
+    
+    cement = pd.read_sql('select * from cement', conn)
+    
+    return cement.to_json(orient = 'records')
+
+#Create function for flaring page
+@app.route("/api/v1.0/flaring")
+def flaring():
+    conn = psycopg2.connect(conn_string)
+    
+    flaring = pd.read_sql('select * from flaring', conn)
+    
+    return flaring.to_json(orient = 'records')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- emissions_postgres_schema.sql
+++ emissions_postgres_schema.sql
@@ -1,5 +1,11 @@
 --create tables from data
 DROP TABLE emissions;
+DROP TABLE total;
+DROP TABLE coal;
+DROP TABLE oil;
+DROP TABLE gas;
+DROP TABLE cement;
+DROP TABLE flaring;
 
 --create main table with emissions data
 CREATE TABLE emissions(
@@ -15,3 +21,84 @@ CREATE TABLE emissions(
 
 SELECT * 
 FROM emissions
+
+
+--create total table 
+CREATE TABLE total(
+		country VARCHAR NOT NULL, 
+		total FLOAT,
+		PRIMARY KEY (country)
+);
+
+SELECT *
+FROM total
+
+--create coal table 
+CREATE TABLE coal(
+		country VARCHAR NOT NULL, 
+		coal FLOAT, 
+		FOREIGN KEY (country) REFERENCES total (country),
+		PRIMARY KEY (country)
+);
+
+SELECT *
+FROM coal
+
+--create oil table
+CREATE TABLE oil(
+		country VARCHAR NOT NULL,
+		oil FLOAT, 
+		FOREIGN KEY (country) REFERENCES total (country),
+		PRIMARY KEY (country)
+);
+
+SELECT *
+FROM oil
+
+--create gas table
+CREATE TABLE gas(
+		country VARCHAR NOT NULL,
+		gas FLOAT, 
+		FOREIGN KEY (country) REFERENCES total (country),
+		PRIMARY KEY (country)
+);
+
+SELECT *
+FROM gas
+
+--create cement table
+CREATE TABLE cement(
+		country VARCHAR NOT NULL,
+		cement FLOAT, 
+		FOREIGN KEY (country) REFERENCES total (country),
+		PRIMARY KEY (country)
+);
+
+SELECT * 
+FROM cement
+
+--create flaring table
+CREATE TABLE flaring(
+		country VARCHAR NOT NULL,
+		flaring FLOAT, 
+		FOREIGN KEY (country) REFERENCES total (country),
+		PRIMARY KEY (country)
+);
+
+SELECT * 
+FROM flaring
+
+CREATE TABLE final(
+		country VARCHAR NOT NULL, 
+		total FLOAT, 
+		coal FLOAT, 
+		oil FLOAT, 
+		gas FLOAT,
+		cement FLOAT, 
+		flaring FLOAT,
+		FOREIGN KEY (country) REFERENCES total (country),
+		PRIMARY KEY (country)
+);
+
+SELECT *
+FROM final
