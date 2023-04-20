49e6f62fed77b5dc72e3686a6e99cb68c9b436fd
spmims <stephenpmims@gmail.com>
2023-04-19 19:04:24 -0500

    attempting to set up

--- static/js/logic.js
+++ static/js/logic.js
@@ -188,6 +188,36 @@ function buildCharts(row) {
       var resultArray = metadata.filter(sampleObj => sampleObj.country == row);
       var result = resultArray[0];
       });
+
+      fetch('https://unpkg.com/world-atlas/countries-50m.json').then((r) => r.json()).then((data) => {
+      const countries = ChartGeo.topojson.feature(data, data.objects.countries).features;
+
+  const chart = new Chart(document.getElementById("canvas").getContext("2d"), {
+    type: 'choropleth',
+    data: {
+      labels: countries.map((d) => d.properties.name),
+      datasets: [{
+        label: 'Countries',
+        data: countries.map((d) => ({feature: d, value: Math.random()})),
+      }]
+    },
+    options: {
+      showOutline: true,
+      showGraticule: true,
+      plugins: {
+        legend: {
+          display: false
+        },
+      },
+      scales: {
+        projection: {
+          axis: 'x',
+          projection: 'equalEarth'
+        }
+      }
+    }
+  });
+});
   
     }
 
